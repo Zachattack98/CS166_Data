f567640e163ef2dbd71779c6de6010f872ae3632
Zachattack98 <bakugan0498@yahoo.com>
2022-03-21 03:05:56 -0700

    Create triggers.sql

---
+++ lab7/triggers.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE part_number_seq START WITH 50000;
+
+CREATE LANGUAGE plpgsql;
+CREATE OR REPLACE FUNCTION part_func() RETURNS "trigger" AS
+   $BODY$
+   BEGIN
+      NEW.part_number := nextval('part_number_seq');
+      RETURN NEW;
+   END;
+   $BODY$
+ LANGUAGE plpgsql VOLATILE;
+
+CREATE TRIGGER part_nyc_trigger BEFORE INSERT ON part_nyc 
+   FOR EACH ROW EXECUTE PROCEDURE part_func();
